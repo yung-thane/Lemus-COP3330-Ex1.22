0532f973f37647ab9afa543fc5de35a7cb644df9
Ronald <ronald.cristobal.lemus@outlook.com>
2021-09-13 19:48:52 -0400

    Initial commit

---
+++ src/Comparing_Numbers.java
@@ -0,0 +1,18 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 1.18 Solution
+ *  Copyright 2021 Ronald Lemus
+ */
+
+import java.util.Scanner;
+
+public class Comparing_Numbers {
+    public static void main(String [] arg){
+        System.out.print("Enter the first number: ");
+        Scanner firstInp = new Scanner(System.in);
+        int firstNum = firstInp.nextInt();
+
+        System.out.print("Enter the second number: ");
+        Scanner secondInp = new Scanner(System.in);
+        int secondNum = secondInp.nextInt();
+    }
+}
